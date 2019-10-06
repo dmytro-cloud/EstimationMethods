d90d077f6016a9baa004985a4d514957973f6761
Dmytro Minchenko <dmytro.minchenko@desy.de>
2019-10-06 20:29:01 +0300

    Parser implemented

---
+++ part1/parse.py
@@ -0,0 +1,26 @@
+import argparse
+
+# Override ArgumentParser to read not only line by line
+class MyArgumentParser(argparse.ArgumentParser):
+    def convert_arg_line_to_args(self, arg_line):
+        return arg_line.split()
+
+class ParseThings:
+
+    # Define parser
+    def __init__(self):   
+        self.parser = MyArgumentParser(fromfile_prefix_chars='@')
+
+        # Define arguments
+        self.parser.add_argument('--comment', nargs='*', help='Comment in file')
+        self.parser.add_argument('--line', dest='lines', nargs=3, action='append', type=float,
+            help='Add spectrum line with Energy, Intensity and deltaE(FWHM)')
+        self.parser.add_argument('--background', dest='background', nargs=4, type=float, default = [1., 2., 3., 4.],
+            help='background parameters: par[0] * exp( par[1] * x) + par[2] + par[3] * x')
+        self.parser.add_argument('--E_max', dest='e_max', type=float, help='Define maximal spectrum energy', default=1.)
+        self.parser.add_argument('--chanels', dest='chanels_num', type=int, help='Number of chanels', default=1000)
+        self.parser.add_argument('--E0', dest='E0', type=float, help='Energy shift', default=0.01)
+
+    # Parse things method
+    def Parse(self):
+        return self.parser.parse_args()
